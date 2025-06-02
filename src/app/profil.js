f134b5ebf8e3660ac155832473e2129fa6bcfc59
jcailly <jeremy.cailly@hotmail.fr>
2025-06-02 23:24:52 +0200

    implement tiptools for stack, creation of robots.txt and sitemap.xml

--- src/app/profil.js
+++ src/app/profil.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link.js";
 import { Button } from "@/components/ui/button.jsx";
+import StackLogos from "@/components/ui/stack";
 
 export default function Profil() {
     return (
@@ -25,35 +26,7 @@ export default function Profil() {
             Développeur d'applications en reconversion après une carrière de 10 ans dans le secteur automobile premium, je mets aujourd’hui mon sens du service, ma rigueur et ma capacité d’adaptation au service de projets numériques. Actuellement en formation à Zone01 Normandie, j’apprends à concevoir et développer des applications web robustes, évolutives et centrées utilisateur, en suivant une pédagogie par projets et en autonomie.
         </p>
         </div>
-        <div className="bg-[#F7F9FF] dark:bg-[#11131F] p-4 ml-8 mr-8 shadow-md rounded-[8px]">
-        <h2 className="text-xl flex mb-2">Stack :</h2>
-        <div className="items-center justify-center gap-[10px] flex flex-wrap text-sm text-justify text-center text-center">
-            <Link href="https://go.dev/" target="_blank">
-            <Image src="/Golang_logo.png" width={75} height={75} alt="Golang logo" className="p-2"/>
-            </Link>
-            <Link href="https://www.rust-lang.org/fr" target="_blank">
-            <Image src="/Rust_logo.png" width={75} height={75} alt="Rust logo" className="p-2"/>
-            </Link>
-            <Link href="https://www.javascript.com/" target="_blank">
-            <Image src="/Javascript_colored_logo.png" width={75} height={75} alt="Javascript logo" className="p-2"/>
-            </Link>
-            <Link href="https://fr.react.dev/" target="_blank">
-            <Image src="/React_colored_logo.png" width={75} height={75} alt="React logo" className="logo-react p-2"/>
-            </Link>
-            <Link href="https://nextjs.org/" target="_blank">
-            <Image src="/next.svg" width={75} height={75} alt="Next.js logo" className="dark:invert p-2"/>
-            </Link>
-            <Link href="https://tailwindcss.com/" target="_blank">
-            <Image src="/Tailwind_colored_logo.png" width={75} height={75} alt="Tailwindcss logo" className="p-2 dark:invert"/>
-            </Link>
-            <Link href="https://www.sqlite.org/" target="_blank">
-            <Image src="/SQLite_logo.png" width={100} height={100} alt="SQLite logo" className="p-2"/>
-            </Link>
-            <Link href="https://git-scm.com/" target="_blank">
-            <Image src="/Git_icon.svg.png" width={75} height={75} alt="Git logo" className="p-2"/>
-            </Link>
-        </div>
-        </div>
+        <StackLogos />
         <Button variant="personal">Téléchargez mon CV</Button>
         <p className="text-sm text-justify text-center ml-8 mr-8 text-center">
             PARCOURS PROFESSIONNEL
---
+++ src/components/ui/stack.js
@@ -0,0 +1,100 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+export default function StackLogos() {
+    return (
+        <TooltipProvider>
+        <div className="bg-[#F7F9FF] dark:bg-[#11131F] p-4 ml-8 mr-8 shadow-md rounded-[8px]">
+        <h2 className="text-xl flex mb-2">Stack :</h2>
+        <div className="items-center justify-center gap-[10px] flex flex-wrap text-sm text-justify text-center text-center">
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://go.dev/" target="_blank">
+                        <Image src="/Golang_logo.png" width={75} height={75} alt="Golang logo" className="p-2"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Golang</p>
+                </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://www.rust-lang.org/fr" target="_blank">
+                        <Image src="/Rust_logo.png" width={75} height={75} alt="Rust logo" className="p-2"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Rust</p>
+                </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://www.javascript.com/" target="_blank">
+                        <Image src="/Javascript_colored_logo.png" width={75} height={75} alt="Javascript logo" className="p-2"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Javascript</p>
+                </TooltipContent>
+            </Tooltip>
+            
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://fr.react.dev/" target="_blank">
+                        <Image src="/React_colored_logo.png" width={75} height={75} alt="React logo" className="logo-react p-2"/>
+                    </Link>
+                    </TooltipTrigger>
+                <TooltipContent>
+                    <p>React</p>
+                </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://nextjs.org/" target="_blank">
+                        <Image src="/next.svg" width={75} height={75} alt="Next.js logo" className="dark:invert p-2"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>NextJS</p>
+                </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://tailwindcss.com/" target="_blank">
+                        <Image src="/Tailwind_colored_logo.png" width={75} height={75} alt="Tailwindcss logo" className="p-2 dark:invert"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>TailwindCSS</p>
+                </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://www.sqlite.org/" target="_blank">
+                        <Image src="/SQLite_logo.png" width={100} height={100} alt="SQLite logo" className="p-2"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>SQLite</p>
+                </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href="https://git-scm.com/" target="_blank">
+                        <Image src="/Git_icon.svg.png" width={75} height={75} alt="Git logo" className="p-2"/>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Git</p>
+                </TooltipContent>
+            </Tooltip>
+        </div>
+        </div>
+        </TooltipProvider>
+    )
+}
