40cf2508dd781d23311fbc6c2d1eb9e2ee73f3ac
jcailly <jeremy.cailly@hotmail.fr>
2025-06-03 13:11:34 +0200

    fix section element size

--- src/app/profil.js
+++ src/app/profil.js
@@ -12,8 +12,8 @@ export default function Profil() {
         flex-none flex-auto
         w-screen h-screen
         overflow-auto scrollbar-hide">
-            <div className="w-full flex justify-center ">
-                <div className="max-w-[800px] flex flex-col gap-[32px] row-start-2 items-center justify-center">
+            <div className="flex justify-center p-4">
+                <div className="max-w-[800px] w-full flex flex-col gap-[32px] row-start-2 items-center justify-center">
                     <Image 
                         className="rounded-full"
                             src="/profil.jpg"
@@ -22,16 +22,17 @@ export default function Profil() {
                             height={38}
                     />
                 <h1 className="text-2xl text-center">Jeremy Cailly<br />Développeur Full-Stack</h1>
-                <div className="bg-[#F7F9FF] dark:bg-[#11131F] shadow-md p-4 ml-8 mr-8 rounded-[8px]">
+                <div className="bg-[#F7F9FF] dark:bg-[#11131F] shadow-md w-full p-4 ml-8 mr-8 rounded-[8px]">
                     <h2 className="text-xl flex mb-2">A propos de moi:</h2>
                     <p className="text-sm text-justify text-center text-center">
                         Développeur d'applications en reconversion après une carrière de 10 ans dans le secteur automobile premium à Rouen, je mets aujourd’hui mon sens du service, ma rigueur et ma capacité d’adaptation au service de projets numériques. Actuellement en formation à Zone01 Normandie, j’apprends à concevoir et développer des applications web robustes, évolutives et centrées utilisateur, en suivant une pédagogie par projets et en autonomie.
                     </p>
                 </div>
                 <StackLogos />
-                <div className="bg-[#F7F9FF] dark:bg-[#11131F] shadow-md p-4 ml-8 mr-8 rounded-[8px]">
+                <div className="bg-[#F7F9FF] dark:bg-[#11131F] shadow-md w-full p-4 ml-8 mr-8 rounded-[8px]">
                     <h2 className="text-xl flex mb-2">Certifications:</h2>
                     <ScrollArea className="h-[200px] rounded-md border-[1px] border-white">
+                        <h3 className="p-4 border-white border-[1px]">Titre RNCP Concepteur Développeur d'Applications (Obtention Nov 2025)</h3>
                         <Link
                             href="https://nextjs.org/learn/certificate?course=react-foundations&user=64283&certId=react-foundations-64283-1748901888572"
                             target="_blank">
--- src/components/ui/stack.js
+++ src/components/ui/stack.js
@@ -6,7 +6,7 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 export default function StackLogos() {
     return (
         <TooltipProvider>
-        <div className="bg-[#F7F9FF] dark:bg-[#11131F] p-4 ml-8 mr-8 shadow-md rounded-[8px]">
+        <div className="bg-[#F7F9FF] dark:bg-[#11131F] p-4 ml-8 mr-8 w-full shadow-md rounded-[8px]">
         <h2 className="text-xl flex mb-2">Stack:</h2>
         <div className="items-center justify-center gap-[10px] flex flex-wrap text-sm text-justify text-center text-center">
             <Tooltip>
