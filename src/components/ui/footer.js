2dbd500c3245d5d2b15696d38e931ba015e92c28
jcailly <jeremy.cailly@hotmail.fr>
2025-06-06 16:43:08 +0200

    fix footer h6 error for lighthouse

--- src/components/ui/footer.js
+++ src/components/ui/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <div className="bg-[#F7F9FF] dark:bg-[#11131F] flex items-center justify-center">
-        <h6 className="font-extralight">© 2025 – Jérémy CAILLY</h6>
+        <p className="font-extralight">© 2025 – Jérémy CAILLY</p>
       </div>
     )
 }
